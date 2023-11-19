8d90ceae4dcf22b85d6c3c11778c335360e133c7
Denowich <denowich@gmail.com>
2023-11-19 23:14:48 +0200

    first commit

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,42 +1,11 @@
 import React from "react";
 
 
-export type TasksType = {
-  id: number
-  title: string
-  isDone: boolean
-}
 
-type PropsType = {
-  title: string
-  tasks: Array<TasksType>
-  removeTask: (taskId: number)=>void
-}
-
-export function Todolist(props: PropsType) {
+export function Todolist() {
   return (
     <div>
-      <h3>{props.title}</h3>
-      <div>
-        <input />
-        <button>+</button>
-      </div>
-      <ul>
-        {
-          props.tasks.map( (t)=> 
-            <li>
-              <input type="checkbox" checked={t.isDone} />
-              <span>{t.title}</span>
-              <button onClick={()=> {props.removeTask(t.id)}}>x</button>
-            </li>
-          )
-        }
-      </ul>
-      <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
-      </div>
+      <h2>Hello Todo!</h2>
     </div>
   )
 }
